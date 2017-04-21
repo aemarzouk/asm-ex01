41df8ab849978becd1e5356b4bcf36ef42cfd3ef
Ahmed Marzouk <amarzouk@eg.ibm.com>
2017-04-21 12:21:14 -0500

    Fix day offset

--- hellotoday.js
+++ hellotoday.js
@@ -3,9 +3,10 @@ var port = (process.env.VCAP_APP_PORT || 8080);
 var http = require('http');
 
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var today = new Date();
 
 var server = http.createServer( function(request, response) {
-	var body = 'Hello ' + days[new Date().getDay()];
+	var body = 'Hello ' + days[ today.getDay() -1 ];
 	response.writeHead( 200, {
 		'Content-Length': body.length,
 		'Content-Type': 'text/plain'
